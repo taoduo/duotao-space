5f1c3229ab77d9b23e9ca40ecbad3c10090d32d0
Duo Tao <taod@carleton.edu>
2018-05-03 17:30:58 -0500

    cdns

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 <head>
 	<link rel="shortcut icon" href="favicon.ico" />
 	<title>Duo Tao - All People of Action Are Dreamers.</title>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body class="container">
 <div id="content">
     <img src="profile.jpg"/>
 </div>
 <h1>Duo Tao</h1>
-
-<p>This is a paragraph.</p>
+<p>I study physics and computer science at Carleton College.</p>
 
 </body>
 </html>
