36d368bb3fdeb0e9283ce522283597c879625d3e
Duo Tao <taod@carleton.edu>
2018-05-03 17:39:46 -0500

    title text

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<link rel="shortcut icon" href="favicon.ico" />
-	<title>Duo Tao - All People of Action Are Dreamers.</title>
+	<title>Duo Tao - What we do for ourselves dies with us. What we do for others and the world remains and is immortal.</title>
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
@@ -12,8 +12,12 @@
 <div id="content">
     <img src="profile.jpg"/>
 </div>
-<h1>Duo Tao</h1>
-<p>I study physics and computer science at Carleton College.</p>
+<center>
+	<h1>Duo Tao</h1>
+	<i>What we do for ourselves dies with us. What we do for others and the world remains and is immortal.</i>
+	<p>I study physics and computer science at Carleton College. </p>
+</center>
+
 
 </body>
 </html>
