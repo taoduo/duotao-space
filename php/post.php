b1ccc92a0ac24ad4cc26fe15a5952be02a390738
Duo Tao <taod@carleton.edu>
2018-05-03 23:41:58 -0500

    modal

--- php/post.php
+++ php/post.php
@@ -4,7 +4,7 @@ $filename = basename($_FILES["file"]["name"]);
 $target_file = $target_dir . $filename;
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+echo 'haha';
 if(isset($_POST["submit"])) {
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         # echo "The file " . basename( $_FILES["file"]["name"]). " has been uploaded.";
