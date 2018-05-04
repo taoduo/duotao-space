11515c40ada968718f8279cb419e08f2a58a7149
Duo Tao <taod@carleton.edu>
2018-05-04 00:05:08 -0500

    modal

--- php/post.php
+++ php/post.php
@@ -5,6 +5,6 @@ $filename = basename($_FILES["file"]["name"]);
 $target_file = $target_dir . $filename;
 if($_SESSION['login'] == 1) {
 	$err = move_uploaded_file($_FILES["file"]["tmp_name"]);
-    echo $err;
+    echo (string)$err;
 }
 ?>
