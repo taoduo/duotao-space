5dc6cd98a5c8fbe8a3cba8f96adeb3af683ddd74
Duo Tao <taod@carleton.edu>
2018-05-03 23:39:10 -0500

    modal

--- php/post.php
+++ php/post.php
@@ -7,7 +7,8 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-        echo "The file " . basename( $_FILES["file"]["name"]). " has been uploaded.";
+        # echo "The file " . basename( $_FILES["file"]["name"]). " has been uploaded.";
+        echo "haha"
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
