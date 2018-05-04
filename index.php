35c7b1231e29d83ab2476c93fc448c264aaa756b
Duo Tao <taod@carleton.edu>
2018-05-03 22:19:22 -0500

    post modal

--- index.php
+++ index.php
@@ -10,19 +10,6 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body class="container">
-	<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">New Post</h3>
-    </div>
-    <div class="modal-body">
-        <p>One fine body…</p>
-    </div>
-    <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-        <button class="btn btn-primary">Save changes</button>
-    </div>
-</div>
 <center>
 	<img height="125" width="125" alt="Duo Tao Profile" src="profile.jpg"/>
 	<h1>Duo Tao</h1>
@@ -33,6 +20,25 @@
 <footer>
 	<center><small>&copy; Copyright 2018, Duo Tao</small></center>
 </footer>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 <script type="text/javascript">
 	var code = ""
 	$("body").keypress(function(e) {
