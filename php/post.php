8bd01ca1547f41f94d32ab9a8ee80bad33faef06
Duo Tao <taod@carleton.edu>
2018-05-04 00:20:42 -0500

    modal

--- php/post.php
+++ php/post.php
@@ -3,7 +3,7 @@ session_start();
 $target_dir = "../posts/";
 $filename = basename($_FILES["file"]["name"]);
 $target_file = $target_dir . $filename;
-if(isset($_FILES['file']) {
+#if(isset($_FILES['file']) {
 	if($_SESSION['login'] == 1) {
 		$moved = move_uploaded_file($_FILES["file"]["tmp_name"]);
 	    if ($moved) {
@@ -12,7 +12,7 @@ if(isset($_FILES['file']) {
 	    	echo "Not uploaded because of error #".$_FILES["file"]["error"];
 	    }
 	}
-} else {
-	echo "No file!";
-}
+#} else {
+#	echo "No file";
+#}
 ?>
