8d603e51ee61bd22da522fc5c4c64dff7486360c
Duo Tao <taod@carleton.edu>
2018-05-03 23:52:59 -0500

    modal

--- php/post.php
+++ php/post.php
@@ -3,7 +3,7 @@ $target_dir = "../posts/";
 $filename = basename($_FILES["file"]["name"]);
 $target_file = $target_dir . $filename;
 echo 'haha';
-if($_SESSION['login'] == 1 && isset($_POST["submit"])) {
+if($_SESSION['login'] == 1) {
 	echo 'hoho';
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         # echo "The file " . basename( $_FILES["file"]["name"]). " has been uploaded.";
