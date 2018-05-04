38e63e5bdb41d5b5feb541b3297f527da0a08912
Duo Tao <taod@carleton.edu>
2018-05-03 23:16:28 -0500

    modal

--- index.php
+++ index.php
@@ -49,12 +49,14 @@
       		e.preventDefault();
       		$('#form-submit').prop('disabled', true);
     		var formData = new FormData(this);
+    		console.log('123');
 		    $.ajax({
 		        url: '/php/post.php',
 		        type: 'POST',
 		        data: formData,
 		        success: function(msg) {
 		            console.log(msg);
+		            console.log('345');
 		        }               
 		    });
 		});
