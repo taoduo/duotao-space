68a7a349ee962e10c2b344c2ced15f0f51a51110
Duo Tao <taod@carleton.edu>
2018-05-03 22:44:41 -0500

    post modal

--- index.php
+++ index.php
@@ -24,13 +24,20 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Create new post</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <div class="form-group">
+	      <label for="title">Title</label>
+	      <input class="form-control" id="title" placeholder="Enter title">
+	    </div>	    
+	    <div class="form-group">
+		    <label for="file">File</label>
+		    <input type="file" class="form-control-file" id="file">
+  		</div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
