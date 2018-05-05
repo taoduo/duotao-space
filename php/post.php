f3b74a94efe1d9aa58bd508d3af32ee668bd88bb
Duo Tao <taod@carleton.edu>
2018-05-04 23:09:53 -0500

    post

--- php/post.php
+++ php/post.php
@@ -6,8 +6,22 @@ $target_file = $target_dir . $filename;
 if($_SESSION['login'] == 1) {
 	$moved = move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
     if ($moved) {
-    	echo "Successfully uploaded";         
+    	$mysqli = new mysqli("localhost", "duotaosp_WPU6Q", "Bonanza2018", "duotaosp_WPU6Q");
+    	if ($conn->connect_error) {
+    		http_response_code(500);
+		    echo "Connection failed: " . $conn->connect_error;
+		} else {
+	    	$statement = $mysqli->prepare("insert into posts (file_path, post_title, create_date) values (?,?,?)");
+	    	$statement->bind_param($file_path, $title, $create_date);
+	    	$file_path = $target_file;
+	    	$title = $_POST['title'];
+	    	$create_date = date('Y-m-d');
+	    	$statement->execute();	    	
+	    	http_response_code(200);
+	    	echo "Success";
+    	}
     } else {
+    	http_response_code(500);
     	echo "Not uploaded because of error #".$_FILES["file"]["error"];
     }
 }
