cf849189b813fa2c7f634d4fca70994bc90d4c32
Duo Tao <taod@carleton.edu>
2018-05-05 13:53:45 -0500

    search

--- index.php
+++ index.php
@@ -28,8 +28,7 @@
     	$('#search-btn').click(function() {
     		var keywords = $('#searchBox').val().split(' ');
     		$('.post').each(function() {
-    			var title = $(this).find('.card-body').length;
-    			// find('post-title').find('a').text();
+    			var title = $(this).find('.card-body').find('.post-title').find('a').text();
     			console.log(title);
     		});
     	});
