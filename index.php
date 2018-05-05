eb6d51f8911d19cf51d4864d681efeef207808de
Duo Tao <taod@carleton.edu>
2018-05-05 13:00:56 -0500

    posts display

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 			  echo ('execute() failed: ' . $stmt->error);
 			} else {
 	    		while($row = $result->fetch_assoc()) {
-	    			echo '<div class="card">';
+	    			echo '<div class="card" style="margin-top:10px">';
 					echo '<div class="card-body">';
 	    			echo '<small>' . $row['create_date'] . '</small>';
 					echo '<h1 style="margin-top:0"><a href="' . $row['file_path'] . '"> ' . $row['post_title'] . " </a></h1>";
