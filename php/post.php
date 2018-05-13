91d741665547a790a7d1d6e0c77be458b28a7d11
Duo Tao <taod@carleton.edu>
2018-05-12 19:48:59 -0500

    file exist alert

--- php/post.php
+++ php/post.php
@@ -9,8 +9,6 @@ $target_file = $target_dir . $filename;
 if($_SESSION['login'] == 1) {
 	if (file_exists($target_file)) {
 		http_response_code(400);
-		$content_abstract = $_POST['abstract'];
-		echo $content_abstract;
 		echo "File exists. Rename your pdf.";
 	} else {
 		$moved = move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
@@ -20,11 +18,11 @@ if($_SESSION['login'] == 1) {
 	    		http_response_code(500);
 			    echo "Connection failed: " . $conn->connect_error;
 			} else {
-		    	$statement = $mysqli->prepare("insert into posts (file_path, post_title, create_date) values (?,?,?);");
+		    	$statement = $mysqli->prepare("insert into posts (file_path, post_title, create_date, content_abstract) values (?,?,?,?);");
 		    	if ( false===$statement ) {
 				  die ('prepare() failed: ' . $mysqli->error);
 				}
-		    	$result = $statement->bind_param('sss', $file_path, $title, $create_date);
+		    	$result = $statement->bind_param('ssss', $file_path, $title, $create_date, $content_abstract);
 		    	if ( false===$result ) {
 				  die('bind_param() failed');
 				}
@@ -32,7 +30,6 @@ if($_SESSION['login'] == 1) {
 		    	$title = $_POST['title'];
 		    	$create_date = date('Y-m-d');
 		    	$content_abstract = $_POST['abstract'];
-		    	echo $content_abstract;
 		    	$result = $statement->execute();
 		    	if ( false===$result ) {
 				  die('execute() failed: ' . $stmt->error);
